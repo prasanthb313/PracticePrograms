f144157c51272b3d645cb9209f94455ec146a039
prasanthb313@gmail.com <prasanthb313@gmail.com>
2019-12-13 15:56:51 +0530

    new code

---
+++ pattern/programs/Inverted.java
@@ -0,0 +1,31 @@
+package com.pattern.programs;
+
+public class Inverted {
+public static void main(String[] args) {
+	int n=10;
+	int space=(n-1)/2;
+	int star=1;
+	for(int i=0;i<=n;i++)
+	{
+		for(int j=0;j<=space;j++)
+		{
+			System.out.print(" ");
+		}
+		for(int k=1;k<=star;k++)
+		{
+			System.out.print("*");
+		}
+		if(i<(n+1)/2)
+		{
+			space--;
+			star+=2;
+		}
+		else {
+			space++;
+			star-=2;
+		}
+		System.out.println(" ");
+	}
+}
+}
+
