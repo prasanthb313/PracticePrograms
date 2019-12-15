90de05169c1d9ee6b623893e5bb4ce27d1c84b70
prasanthb313@gmail.com <prasanthb313@gmail.com>
2019-12-15 13:47:54 +0530

    modified code

--- strings/pages/OccuranceOfEachCharacterInString.java
+++ strings/pages/OccuranceOfEachCharacterInString.java
@@ -1,7 +1,10 @@
 package com.strings.pages;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 
 public class OccuranceOfEachCharacterInString {
 public static void main(String[] args) {
@@ -17,6 +20,14 @@ public static void main(String[] args) {
 		else
 			count.put(c, 1);
 	}
+	Set<Entry<Character, Integer>> set=count.entrySet();
+	Iterator<Entry<Character, Integer>> itr=set.iterator();
+	while(itr.hasNext()){
+		Entry<Character, Integer> entry=itr.next();
+		System.out.println(entry.getKey()+"   "+entry.getValue());
+	}
+	System.out.println("another iteration method....................");
+	
 	for(Map.Entry<Character, Integer> entry:count.entrySet())
 	{
 		System.out.println(" "+entry.getKey()+"   character occured times is  "+entry.getValue());
